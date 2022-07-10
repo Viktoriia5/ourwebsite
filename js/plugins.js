4bbb6840e0b8b3845a6103c18fccd5f8fe1c4f5d
Viktoriia Shpak <viktoriia.ienko@gmail.com>
2022-07-10 20:24:48 +0300

    first committ

---
+++ js/plugins.js
@@ -0,0 +1,149 @@
+/*========== NAVBAR TRANSPARENT TO SOLID ==========*/
+
+$(window).scroll(checkScroll);
+function checkScroll() {
+  if ($(window).scrollTop() >= 140) {
+    $(".navbar").addClass("solid");
+  } else {
+    $(".navbar").removeClass("solid");
+  }
+}
+
+/*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
+
+$(document).ready(function () {
+  checkScroll();
+  $(window).scroll(checkScroll);
+  $(".navbar-toggler").click(function () {
+    if ($(window).scrollTop() <= 300) {
+      $("nav.navbar").toggleClass("solid-toggle");
+    }
+  });
+});
+
+/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $(".navbar-toggler").addClass("collapsed");
+  $("#navbarResponsive").removeClass("show");
+
+  setTimeout(function () {
+    $("nav.navbar").removeClass("solid-toggle");
+  }, 300);
+
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    1000
+  );
+});
+/*========== BOUNCING DOWN ARROW ==========*/
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+});
+
+/*========== LIGHTBOX IMAGE GALLERY ==========*/
+
+/*========== MEET THE TEAM CAROUSEL ==========*/
+$(document).ready(function () {
+  //when document(DOM) loads completely
+  $("#team-carousel").owlCarousel({
+    //owlCarousel settings
+    autoplay: true, //set to false to turn off autoplay and only use nav
+    autoplayHoverPause: true, //set to false to prevent pausing on hover
+    loop: true, //set to false to stop carousel after all slides shown
+    autoplayTimeout: 7000, //time between transitions
+    smartSpeed: 1200, //transition speed
+    dotsSpeed: 1000, //transition speed when using dots/buttons
+    responsive: {
+      //set number of items shown per screen width
+      0: {
+        items: 1, //0px width and up display 1 item
+      },
+      768: {
+        items: 2, //768px width and up display 2 items
+      },
+      992: {
+        items: 3, //992px width and up display 3 items
+      },
+    },
+  });
+});
+
+/*========== SKILLS COUNTER ==========*/
+
+$(document).ready(function () {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 3000,
+  });
+});
+/*========== CLIENTS CAROUSEL ==========*/
+
+$(document).ready(function () {
+  //when document(DOM) loads completely
+  $("#clients-carousel").owlCarousel({
+    //owlCarousel settings
+    autoplay: true, //set to false to turn off autoplay and only use nav
+    autoplayHoverPause: true, //set to false to prevent pausing on hover
+    loop: true, //set to false to stop carousel after all slides shown
+    autoplayTimeout: 7000, //time between transitions
+    smartSpeed: 1600, //transition speed
+    dotsSpeed: 1000, //transition speed when using dots/buttons
+    responsive: {
+      //set number of items shown per screen width
+      0: {
+        items: 1, //0px width and up display 1 item
+      },
+      768: {
+        items: 2, //768px width and up display 2 items
+      },
+    },
+  });
+});
+/*========== TOP SCROLL BUTTON ==========*/
+
+/*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE 
+
+**Remember to use "animate__animated animate__fadeInUp" ==========*/
+
+/*========== WAYPOINTS ANIMATION DELAY ==========*/
+//Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
+$(function () {
+  // a self calling function
+  function onScrollInit(items, trigger) {
+    // a custom made function
+    items.each(function () {
+      //for every element in items run function
+      var osElement = $(this), //set osElement to the current
+        osAnimationClass = osElement.attr("data-animation"), //get value of attribute data-animation type
+        osAnimationDelay = osElement.attr("data-delay"); //get value of attribute data-delay time
+
+      osElement.css({
+        //change css of element
+        "-webkit-animation-delay": osAnimationDelay, //for safari browsers
+        "-moz-animation-delay": osAnimationDelay, //for mozilla browsers
+        "animation-delay": osAnimationDelay, //normal
+      });
+
+      var osTrigger = trigger ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
+
+      osTrigger.waypoint(
+        function () {
+          //scroll upwards and downwards
+          osElement.addClass("animated").addClass(osAnimationClass); //add animated and the data-animation class to the element.
+        },
+        {
+          triggerOnce: true, //only once this animation should happen
+          offset: "70%", // animation should happen when the element is 70% below from the top of the browser window
+        }
+      );
+    });
+  }
+
+  onScrollInit($(".os-animation")); //function call with only items
+  onScrollInit($(".staggered-animation"), $(".staggered-animation-container")); //function call with items and trigger
+});
